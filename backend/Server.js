355f0e9a48798b841112a1d8edb69970b3abab1d
shradha0202 <ksshradha166gmail.com>
2026-09-19 16:05:22 +0530

    Add budget CRUD API

--- backend/Server.js
+++ backend/Server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const transactionRoutes=require("./routes/transactionRoutes");
+const budgetRoutes = require("./routes/budgetRoutes");
 const app = express();
 
 app.use(cors());
@@ -12,6 +13,7 @@ app.use(express.json());
 // Connect to MongoDB
 connectDB();
 app.use("/api/transactions",transactionRoutes);
+app.use("/api/budgets", budgetRoutes);
 
 app.get("/", (req, res) => {
     res.send("Finance Coach Backend is running!");
